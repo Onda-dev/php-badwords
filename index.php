0f34886a25f1e1d92fcae55be722737e3c003283
Onda-dev <manuelaltieri3@gmail.com>
2022-06-21 16:09:27 +0200

    Prima prova ok

--- index.php
+++ index.php
@@ -3,5 +3,38 @@ Stampare a schermo il paragrafo e la sua lunghezza.
 Una parola da censurare viene passata dall'utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
 A domani :php: -->
+<?php
+    $text = "Ciao pezzo di merda, come stai?";
 
+    $array_prova = explode(',', $text);
 
+    $censored_text = str_replace ('merda', '*****', $text);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="hello">
+        <h2>
+            <?php echo $text; ?>
+        </h2>
+        <h3>
+            Lunghezza testo: <?php echo mb_strlen($text); ?>
+        </h3>
+    </div>
+    <div class="censored-hello">
+        <h2>
+            <?php echo $censored_text; ?>
+        </h2>
+        <h3>
+            Lunghezza testo: <?php echo mb_strlen($censored_text) ?>
+        </h3>
+    </div>
+</body>
+</html>
