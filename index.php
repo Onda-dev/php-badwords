b5648470477f39588f16da6f19a11bb6a0b4e5e3
Onda-dev <manuelaltieri3@gmail.com>
2022-06-21 15:41:22 +0200

    Repo creata

---
+++ index.php
@@ -0,0 +1,7 @@
+<!-- Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+A domani :php: -->
+
+
